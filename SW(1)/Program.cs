68279cc4b50c2cd0ea897a07fdc9212448f598f9
Ernest Del Rosario <geloadrian24@gmail.com>
2023-04-22 22:06:14 +0800

    Add project files.

---
+++ SW(1)/Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    public static void Main()
+    {
+
+        String line;
+
+        StreamReader sr = new StreamReader ("C:\\Users\\admin\\Desktop\\Sample.txt");
+
+        line = sr.ReadLine();
+
+        while (line != null)
+        {
+            Console.WriteLine(line);
+            line = sr.ReadLine();
+        }
+
+        sr.Close(); 
+
+        StreamWriter sw = new StreamWriter ("C:\\Users\\admin\\Desktop\\Sample.txt");
+
+        sw.WriteLine("Hello World!");
+        sw.WriteLine("Sample Text");
+        sw.WriteLine("From StreamWriter!");
+
+        sw.Close();
+
+        Console.ReadLine();
+    }
+}
