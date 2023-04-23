d51fc521fef46cb0914949e12f2fbb67b2e353ce
Ernest Del Rosario <geloadrian24@gmail.com>
2023-04-23 20:36:49 +0800

    1.1 update

--- SW(1)/Program.cs
+++ SW(1)/Program.cs
@@ -1,30 +1,29 @@
-﻿internal class Program
+﻿internal class Program // 1.0 - variable changes
 {
     public static void Main()
     {
+        String Line;
+        String cnts;
 
-        String line;
+        // Read the existing contents of the file
+        StreamReader sr = new StreamReader("C:\\Users\\admin\\Desktop\\Sample.txt");
+        Line = sr.ReadToEnd();
+        sr.Close();
 
-        StreamReader sr = new StreamReader ("C:\\Users\\admin\\Desktop\\Sample.txt");
+        Console.WriteLine("Current contents of the file:");
+        Console.WriteLine(Line);
 
-        line = sr.ReadLine();
+        // Append new contents to the file
+        StreamWriter sw = new StreamWriter("C:\\Users\\admin\\Desktop\\Sample.txt", append: true);
 
-        while (line != null)
-        {
-            Console.WriteLine(line);
-            line = sr.ReadLine();
-        }
+        Console.Write("Please enter the contents you want to add to your text file: ");
+        cnts = Console.ReadLine();
 
-        sr.Close(); 
-
-        StreamWriter sw = new StreamWriter ("C:\\Users\\admin\\Desktop\\Sample.txt");
-
-        sw.WriteLine("Hello World!");
-        sw.WriteLine("Sample Text");
-        sw.WriteLine("From StreamWriter!");
+        sw.WriteLine(cnts);
 
         sw.Close();
 
+        Console.WriteLine("Process done! Check your text file to see the changes.");
         Console.ReadLine();
     }
 }
